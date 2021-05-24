897e4e4a23e2d464f0d00f6b9760154597bb2245
Mateusz Czapliński <czapkofan@gmail.com>
2021-05-24 18:27:16 +0200

    feat: parse sample expression

--- main.go
+++ main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"strings"
 
+	"github.com/alecthomas/participle/v2"
 	"github.com/alecthomas/participle/v2/lexer/stateful"
 )
 
@@ -15,21 +19,73 @@ const (
 /*
 	Query = Requirement {`AND` Requirement}
 	Requirement = Check {`OR` Check}
-	Check = Field `=` Pattern | `(` Query `)`
-	Field = Word {`.` Word}
+	Check = Path `=` Pattern | `(` Query `)`
+	Path = Field {`.` Field}
 	Pattern = `"` ... `"`
 */
 
+// TODO: try codegen: participle/experimental/codegen.GenerateLexer()
 var basicLexer = stateful.MustSimple([]stateful.Rule{
 	// {"Comment", `(?i)rem[^\n]*`, nil},
-	// {"String", `"(\\"|[^"])*"`, nil},
+	{"String", `"(\\"|\\\\|[^"])*"`, nil},
 	// {"Number", `[-+]?(\d*\.)?\d+`, nil},
-	// {"Ident", `[a-zA-Z_]\w*`, nil},
-	// {"Punct", `[-[!@#$%^&*()+_={}\|:;"'<,>.?/]|]`, nil},
+	{"Field", `[a-z][a-zA-Z0-9]*`, nil},
+	{"Ident", `[a-zA-Z_]\w*`, nil},
+	{"Punct", `[-[!@#$%^&*()+_={}\|:;"'<,>.?/]|]`, nil},
 	// {"EOL", `[\n\r]+`, nil},
-	// {"whitespace", `[ \t]+`, nil},
+	{"whitespace", `[ \t]+`, nil},
 })
 
+type Query struct {
+	Left  *Requirement      `@@`
+	Right []*AndRequirement `@@*`
+}
+
+type AndRequirement struct {
+	Req *Requirement `"AND" @@`
+}
+
+type Requirement struct {
+	Left  *Check     `@@`
+	Right []*OrCheck `@@*`
+}
+
+type OrCheck struct {
+	Check *Check `"OR" @@`
+}
+
+type Check struct {
+	Cmp      *Comparison `@@`
+	Subquery *Query      `| "(" @@ ")"`
+}
+
+type Comparison struct {
+	Path   *Path   `@@`
+	String *string `"=" @String`
+}
+
+type Path struct {
+	Name *string     `@Field`
+	Sub  []*Subfield `@@*`
+}
+
+type Subfield struct {
+	Name *string `"." @Field`
+}
+
 func main() {
-	fmt.Println("hello")
+	parser, err := participle.Build(&Query{}, participle.Lexer(basicLexer))
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(parser)
+	fmt.Println(strings.Repeat("=", 30))
+
+	var q Query
+	err = parser.ParseString("", ex1, &q)
+	if err != nil {
+		log.Fatal(err)
+	}
+	buf, _ := json.MarshalIndent(q, "", "  ")
+	fmt.Println("AST:", string(buf))
 }
