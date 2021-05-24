24c13988cc2069a24547f26ae65cda847b7a79ae
Mateusz Czapliński <czapkofan@gmail.com>
2021-05-24 17:54:07 +0200

    WIP collecting requirements

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/akavel/participle-test
 
 go 1.14
+
+require github.com/alecthomas/participle/v2 v2.0.0-alpha5
--- main.go
+++ main.go
@@ -1,6 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/alecthomas/participle/v2/lexer/stateful"
+)
+
+// GOAL: parse below samples
+const (
+	ex1 = `github.name = "*foo*" AND github.owner = "*bar*"`
+	ex2 = `github.name = "*foo*"`
+)
+
+/*
+	Query = Requirement {`AND` Requirement}
+	Requirement = Check {`OR` Check}
+	Check = Field `=` Pattern | `(` Query `)`
+	Field = Word {`.` Word}
+	Pattern = `"` ... `"`
+*/
+
+var basicLexer = stateful.MustSimple([]stateful.Rule{
+	// {"Comment", `(?i)rem[^\n]*`, nil},
+	// {"String", `"(\\"|[^"])*"`, nil},
+	// {"Number", `[-+]?(\d*\.)?\d+`, nil},
+	// {"Ident", `[a-zA-Z_]\w*`, nil},
+	// {"Punct", `[-[!@#$%^&*()+_={}\|:;"'<,>.?/]|]`, nil},
+	// {"EOL", `[\n\r]+`, nil},
+	// {"whitespace", `[ \t]+`, nil},
+})
 
 func main() {
 	fmt.Println("hello")
